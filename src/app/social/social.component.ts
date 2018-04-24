5f29b514854fe6816b8fe74e412d4adc1a14afc1
JuanCardona <j1cardona@hotmail.com>
2018-04-24 16:53:32 -0400

    tweeks to links ui

--- src/app/social/social.component.ts
+++ src/app/social/social.component.ts
@@ -13,11 +13,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
   animations: [
     trigger('slideInOut', [
       transition(':enter', [
-        style({transform: 'translateY(-100%)'}),
-        animate('600ms ease-in', style({transform: 'translateY(0%)'}))
+        style({ transform: 'translateY(-100%)' }),
+        animate('600ms ease-in', style({ transform: 'translateY(0%)' }))
       ]),
       transition(':leave', [
-        animate('600ms ease-in', style({transform: 'translateY(-100%)'}))
+        animate('600ms ease-in', style({ transform: 'translateY(-100%)' }))
       ])
     ])
   ]
@@ -41,7 +41,7 @@ export class SocialComponent implements OnInit {
 
   handleShowStats() {
     this.showStats = this.showStats === true ? false : true;
-    if(this.showStats) {
+    if (this.showStats) {
       this.statState = 'Hide'
     } else {
       this.statState = 'Show'
@@ -49,22 +49,14 @@ export class SocialComponent implements OnInit {
   }
 
   getIcon(x) {
-    // Google+    = &#xe900;
-    // Instagram  = &#xe901;
-    // YouTube    = &#xe902;
-    // Twitch     = &#xe903;
-    // Twitter    = &#xe904;
-    // FaceBook   = &#xe905;
-    // SnapChat   = &#xe906;
-
     const list = [
-      {name: 'Facebook', value: '&#xe905;'},
-      {name: 'Twitter', value: '&#xe904;'},
-      {name: 'Twitch', value: '&#xe903;'},
-      {name: 'YouTube', value: '&#xe902;'},
-      {name: 'Instagram', value: '&#xe901;'},
-      {name: 'Google+', value: '&#xe900;'},
-      {name: 'Snapchat', value: '&#xe906;'}
+      { name: 'Facebook', value: '&#xe905;' },
+      { name: 'Twitter', value: '&#xe904;' },
+      { name: 'Twitch', value: '&#xe903;' },
+      { name: 'YouTube', value: '&#xe902;' },
+      { name: 'Instagram', value: '&#xe901;' },
+      { name: 'Google+', value: '&#xe900;' },
+      { name: 'Snapchat', value: '&#xe906;' }
     ]
     let icon
     for (let service of list) {
@@ -72,8 +64,6 @@ export class SocialComponent implements OnInit {
         icon = service.value
       }
     }
-
     return icon
-
   }
 }
